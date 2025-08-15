9720840d90af5f19ee9580245729d1d1d6d1a4bd
pr0ctor <20609138+pr0ctor@users.noreply.github.com>
2025-08-15 15:16:55 -0400

    Removed unnecessary usings

--- Addmusic2/Helpers/FileConverters.cs
+++ Addmusic2/Helpers/FileConverters.cs
@@ -8,7 +8,6 @@ using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Addmusic2.Helpers
 {
