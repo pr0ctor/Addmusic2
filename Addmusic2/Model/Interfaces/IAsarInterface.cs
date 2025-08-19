8437338f56c9d39dbba20f65de4b0f11331a5cf6
pr0ctor <20609138+pr0ctor@users.noreply.github.com>
2025-08-19 05:56:33 -0400

    Added source datafiles Added project reference and config for ILRepack Updated build process to support the new merged assembly Added Asar bindings and assembly Removed unnecessary files and locations Updated Asar interface to include necessary functions for different Asar versions

--- Addmusic2/Model/Interfaces/IAsarInterface.cs
+++ Addmusic2/Model/Interfaces/IAsarInterface.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AsarCLR;
 
 namespace Addmusic2.Model.Interfaces
 {
-    internal interface IAsarInterface
+    internal interface IAsarInterface : IDisposable
     {
+        public Asar191 Asar { get; set; }
+        public bool InitializeAsar();
+        public bool CloseAsar();
+        public string GetAsarVersion();
+        public string GetAsarApiVersion();
     }
 }
